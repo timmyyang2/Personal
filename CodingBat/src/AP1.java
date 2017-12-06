4cf44a65c4153eeedc956cbb2cbb0860e5013e09
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2017-12-05 21:04:19 -0500

    New AP-1

--- CodingBat/src/AP1.java
+++ CodingBat/src/AP1.java
@@ -15,7 +15,7 @@ public class AP1 {
 	public boolean scores100(int[] scores) {
 		  for(int i=0;i<scores.length-1;i++){
 		    if(scores[i] == scores[i+1]){
-		      return true;
+		      return true; 
 		    }
 		  }
 		  return false;

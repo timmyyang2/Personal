f5d505a31eb62763ba35043ca3127f90ccb64d2b
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2017-12-23 12:35:37 -0500

    Added CEquals

---
+++ Algorithm/src/com/dma/CEquals.java
@@ -0,0 +1,25 @@
+package com.dma;
+
+public class CEquals {
+
+	public static int cValue(int c){
+		int k = 0;
+		int s = 1;
+		
+		do{
+			if(s<0){
+				c = c - 1;
+			}else{
+				c = c + 1;
+			}
+			k = k + 1;
+			s = s - 1;
+		}while(k != 8);
+		
+		return c;
+	}
+		
+	public static void main(String[] args){
+		System.out.println("C is " + CEquals.cValue(0));
+	}
+}

f2568ef39edf670a0ac0fc3289daa49d9e7add76
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2017-12-29 17:13:23 -0500

    Added getSandwich

--- CodingBat/src/String2.java
+++ CodingBat/src/String2.java
@@ -137,4 +137,12 @@ public class String2 {
 			  }
 			  return false;
 		}
+		
+		public String getSandwich(String str) {
+			  if(str.indexOf("bread")==str.lastIndexOf("bread")){
+			    return "";
+			  } 
+			  return(str.substring(str.indexOf("bread")+5,str.lastIndexOf("bread")));
+		}
+
 }

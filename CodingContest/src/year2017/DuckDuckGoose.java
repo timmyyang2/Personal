ceaa6f9d60bc2f0144805c6349547b5f97a8f16d
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2017-12-29 15:49:16 -0500

    Added Duck

--- CodingContest/src/year2017/DuckDuckGoose.java
+++ CodingContest/src/year2017/DuckDuckGoose.java
@@ -3,30 +3,68 @@ import java.util.ArrayList;
 
 public class DuckDuckGoose {
 	
-	public static void main(String[] args){
+	public static ArrayList<Integer> ddgList(int n, int k){
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		ArrayList<Integer> printList = new ArrayList<Integer>();
 		
-		String S1 = "1";
-		String S2 = "2";
-		String S3 = "3";
-		String S4 = "4";
-		String S5 = "5";
-		String S6 = "6";
-		String S7 = "7";
-		
-		ArrayList<String> myList = new ArrayList<String>();
-		myList.add(S1);
-		myList.add(S2);
-		myList.add(S3);
-		myList.add(S4);
-		myList.add(S5);
-		myList.add(S6);
-		myList.add(S7);
-		
-		int k = 3;
-		for(int i = 0; i<(k-1); i++){
-			String e = myList.remove(0);
-			myList.add(e);
+		for(int i=1; i<=n; i++){
+			myList.add(i);
 		}
 		
+		for(int i=k; i<=n; i++){
+		int remove = myList.remove(k-1);
+		printList.add(remove);
+		}
+		return printList;
+	}
+	public static void main(String[] args){
+		if(args.length != 2){
+			System.out.println("PASS IN N AND K!!");
+			System.exit(1);
+		}
+		ArrayList myList = DuckDuckGoose.ddgList(Integer.parseInt(args[0]), Integer.parseInt(args[1])); 
+		for(int i=0; i<myList.size(); i++){
+			//System.out.print(myList.get(i) + " ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("31524");
 	}
 }

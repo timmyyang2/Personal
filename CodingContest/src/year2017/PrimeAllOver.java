2c49da2fc1a7fd033fae892fcf99be7fca91967a
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2018-01-04 22:06:43 -0500

    Added PrimeAllOver

---
+++ CodingContest/src/year2017/PrimeAllOver.java
@@ -0,0 +1,41 @@
+package year2017;
+
+public class PrimeAllOver {
+
+	private static boolean isPrime(int n){
+		if(n < 2 || n % 2 == 0){
+			return false;
+		}else{
+			for(int i=2; i<n; i++){
+				if(n % i == 0){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	private static void largestPrime(){
+		String greatestValue = "";
+		for(int i=1000000; i<=9999999; i++){
+			String stringValue = Integer.toString(i);
+			boolean prime = true;
+			for(int j=0; j<stringValue.length(); j++){
+				if(!isPrime(Integer.parseInt(stringValue.substring(0, j+1)))){
+					prime = false;
+					break;
+				}
+			}
+			if(prime){
+				greatestValue = stringValue;
+			}
+		}
+		for(int k=0; k<greatestValue.length(); k++){
+			System.out.println(greatestValue.substring(0, k+1) + " is prime.");
+		}
+	}
+	
+	public static void main(String[] args){
+		PrimeAllOver.largestPrime();
+	}
+}

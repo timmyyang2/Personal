31e3bbaa0893a3715ada40b96dc9af9b836448a7
NBTTECH\yyang <yichong.yang@riverbed.com>
2018-01-17 15:56:06 -0500

    local changes

--- DataAnalysis/src/weather/Test.java
+++ DataAnalysis/src/weather/Test.java
@@ -3,8 +3,5 @@ package weather;
 public class Test {
 	// Add something here.
 	// This is a test. I'm also changing this line.
-=======
-	// This is a test. I'm changing this line.
->>>>>>> 307f57541f4e72f15a017ffabcf20cc9469c0c2c
 	// Test something here.
 }

bc1512c8b3ebb794ca764f5c141f9f8625a76c63
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2018-02-24 20:43:25 -0500

    Latest Development

--- CodingBat/src/Recursion1.java
+++ CodingBat/src/Recursion1.java
@@ -17,5 +17,40 @@ public class Recursion1 {
 		  }
 		  return 2+bunnyEars2(bunnies-1);
 		}
+	
+	public int triangle(int rows) {
+		  if(rows==0){
+		    return 0;
+		  }
+		  return rows+triangle(rows-1);
+		}
+
+	public int sumDigits(int n) {
+		  if(n==0){
+		    return 0;
+		  }
+		  return (n%10)+sumDigits(n/10);
+		}
+
+	public int count7(int n) {
+		  if(n==0){
+		    return 0;
+		  }
+		  if(n%10==7){
+		    return 1+count7(n/10);
+		  }
+		  return count7(n/10);
+		}
+
+	public static int powerN(int base, int n) {
+		  if(n==0){
+		    return 1;
+		  }
+		  return base*powerN(base, n-1);
+		}
+
+	public static void main(String[] args){
+		System.out.println(powerN(10,5));
+	}
 
 }

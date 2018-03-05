e1de4c923a454dc02c363407e1c6626e34253c1c
timothyyang2002 <33989303+timothyyang2002@users.noreply.github.com>
2018-03-05 17:48:08 -0500

    Latest Development

--- CodingBat/src/Recursion1.java
+++ CodingBat/src/Recursion1.java
@@ -237,6 +237,16 @@ public class Recursion1 {
 		  return false;
 		}
 
+	public int strCount(String str, String sub) {
+		   if(sub.length()>str.length()){
+				    return 0;
+				  }
+				  if(str.substring(0,sub.length()).equals(sub)){
+				    return 1+strCount(str.substring(sub.length()), sub);
+				  }
+				  return strCount(str.substring(1), sub);
+		}
+
 	public static void main(String[] args){
 		System.out.println(powerN(10,5));
 	}

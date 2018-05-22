5aba93d058fe3462dc9118f2445d9fdfd68ca7a8
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2018-05-21 22:00:37 -0400

    Latest Development

--- CodingBat/src/HashMap.java
+++ CodingBat/src/HashMap.java
@@ -42,5 +42,56 @@ public class HashMap {
 		  }
 		  return map;
 		}
+	
+	public Map<String, String> topping3(Map<String, String> map) {
+		  if(map.containsKey("potato")){
+		    map.put("fries", map.get("potato"));
+		  }
+		  if(map.containsKey("salad")){
+		    map.put("spinach", map.get("salad"));
+		  }
+		  return map;
+		}
+
+	public Map<String, String> mapAB2(Map<String, String> map) {
+		  if(map.containsKey("a") && map.containsKey("b")){
+		    if(map.get("a").equals(map.get("b"))){
+		      map.remove("a");
+		      map.remove("b");
+		    }
+		  }
+		  return map;
+		}
+
+	public Map<String, String> mapAB3(Map<String, String> map) {
+		  if(map.containsKey("a") && !map.containsKey("b")){
+		    map.put("b", map.get("a"));
+		  }
+		  if(map.containsKey("b") && !map.containsKey("a")){
+		    map.put("a", map.get("b"));
+		  }
+		  return map;
+		}
+	
+	public Map<String, String> mapAB4(Map<String, String> map) {
+		  if(map.containsKey("a") && map.containsKey("b")){
+		    if(map.get("a").length()> map.get("b").length()){
+		      map.put("c", map.get("a"));
+		    }
+		  }
+		  if(map.containsKey("a") && map.containsKey("b")){
+		    if(map.get("b").length()> map.get("a").length()){
+		      map.put("c", map.get("b"));
+		    }
+		  }
+		  if(map.containsKey("a") && map.containsKey("b")){
+		    if(map.get("a").length() == map.get("b").length()){
+		      map.put("a", "");
+		      map.put("b", "");
+		    }
+		  }
+		  return map;
+		}
+
 
 }

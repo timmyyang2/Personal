d1a767e66afcac610efca8674180a246d86d5f43
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2018-07-29 22:50:55 -0400

    Latest Development

--- CodingContest/src/year2011/GenerateCombinations2.java
+++ CodingContest/src/year2011/GenerateCombinations2.java
@@ -11,18 +11,20 @@ public class GenerateCombinations2 {
 		int n = scanner.nextInt();
 		for(int l=1; l<n+1; l++) {
 			alpha = alpha + l;
-			for(int i=0; i<alpha.length(); i++){
-				for(int j=i+1; j<alpha.length(); j++){
-					for(int k=j+1; k<alpha.length(); k++){
-						System.out.print(alpha.charAt(i));
-						System.out.print(alpha.charAt(j));
-						System.out.print(alpha.charAt(k));
-						System.out.println();
-					}
-				}	
-			}
+		}
+		
+		for(int i=0; i<alpha.length(); i++){
+			for(int j=i+1; j<alpha.length(); j++){
+				for(int k=j+1; k<alpha.length(); k++){
+					System.out.print(alpha.charAt(i));
+					System.out.print(alpha.charAt(j));
+					System.out.print(alpha.charAt(k));
+					System.out.println();
+				}
+			}	
 		}
 	}
+	
 	public static void main(String[] args){
 		comb();
 	}

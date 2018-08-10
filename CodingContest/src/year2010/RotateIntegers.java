0b77f70ea1a9135708b853fef398812c26cd7ce7
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2018-08-10 11:33:38 -0400

    Latest Development

--- CodingContest/src/year2010/RotateIntegers.java
+++ CodingContest/src/year2010/RotateIntegers.java
@@ -1,11 +1,24 @@
 package year2010;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RotateIntegers {
 	
 	public static void rotateInt() {
-
+		System.out.print("How many integers? ");
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.print("Input " + input + " integers?");
+		Scanner scannerr = new Scanner(System.in);
+		String number = scannerr.nextLine();
+		System.out.println(number);
+		int count = 1;
+		for(int j=0; j<input-1; j++) {
+			number = number + number.charAt(j);
+			System.out.println(number.substring(count, number.length()));
+			count++;
+		}
 	}
 
 	public static void main(String[] args) {

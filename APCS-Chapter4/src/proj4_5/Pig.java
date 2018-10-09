9b5c1d9546c9ba94323062e53dbaeed597c4bc14
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2018-10-09 19:54:03 -0400

    Latest Development

--- APCS-Chapter4/src/proj4_5/Pig.java
+++ APCS-Chapter4/src/proj4_5/Pig.java
@@ -1,62 +1,61 @@
-package proj4_5;
+package proj4_5; //project package 4.5
 
-public class Pig{
-   private int target;
+public class Pig{ //class
+   private int target; //int target for target score
    private PairOfDice dice;
    private Player computer;
    private Player user;
    private Player current;
    
-   public Pig(int target){
+   public Pig(int target){ //constructor
       this.target = target;
       this.dice = new PairOfDice();
       this.computer = new Player(20);
-      this.user = new Player(Player.ASK);
+      this.user = new Player(Player.END);
    }
 
-   public void game(){
-      boolean win = true;
-      current = computer;
-      do{
-         takeTurn();
-         if(current.getPoints() >= target) {
-            win = false;
+   public void game(){ //game method
+      boolean win = false; //boolean variable win is set as true
+      current = computer; //current = computer because computer starts the game
+      do{ //do while loop
+         swapPlayer(); //run swapPlayer method
+         if(current.getPoints() >= target) { //checks if current player points is greater than 100
+            win = true; //win is set to true
          }else if (current == computer) {
                current = user;
          }else {
                current = computer;
          }
       }
-      while (win == true);
-
-      winner();
+      while (win == false); //run this while win is false
+      winner(); //runs winner method
    }
 
-   private void takeTurn (){
-      boolean stillRolling = true;
-      System.out.println ("____________________");
-      System.out.println ("Current Results:");
-      System.out.println ("Computer: " + computer.getPoints());
-      System.out.println ("You: " + user.getPoints());
-      while (stillRolling) {
-         stillRolling = current.roll (dice, target);
+   private void swapPlayer(){ //swapPlayer method
+      boolean rolling = true; //sets rolling to true
+      System.out.println("____________________"); //divider
+      System.out.println("Current Results:"); //prints current results
+      System.out.println("Computer: " + computer.getPoints()); //prints computer points
+      System.out.println("You: " + user.getPoints()); //prints your points
+      while (rolling == true) { //while rolling is true
+         rolling = current.roll (dice, target); //set rolling to current.roll method
       }
    }
 
-   private void winner (){
-      System.out.println();
-      if (current == computer) {
-         System.out.println ("Computer wins!");
-      }else {
-         System.out.println ("You win!");
+   private void winner(){ //winner method
+      System.out.println(); //space
+      if (current == computer) { //if computer is current player
+         System.out.println("Computer wins!"); //computer wins message
+      }else { //else
+         System.out.println("You win!"); //you win message
       }
-      System.out.println();
-      System.out.println ("Computer's score: " + computer.getPoints());
-      System.out.println ("Your score: " + user.getPoints());
+      System.out.println(); //space
+      System.out.println("Computer score: " + computer.getPoints()); //prints computer points
+      System.out.println("Your score: " + user.getPoints()); //prints your points
    }
    
-   public static void main (String[] args){
-      Pig pig = new Pig(100);
-      pig.game();
+   public static void main (String[] args){ //main method
+      Pig pig = new Pig(100); //new pig object
+      pig.game(); //run the game method
    }
 }
--- APCS-Chapter4/src/proj4_5/Player.java
+++ APCS-Chapter4/src/proj4_5/Player.java
@@ -1,58 +1,56 @@
-package proj4_5;
+package proj4_5; //project package 4.5
 
-import java.util.Scanner;
+import java.util.Scanner; //Scanner for user input
 
-public class Player{
-   public final static int ASK = -1;  // prompt for round termination
-   private int total;  // total points accumulated in game
-   private int round;  // points accumulated in current round
-   private int max;  // pass tolerance
+public class Player{ //class
+   public final static int END = -1;  //for round termination
+   private int total;  //points in total
+   private int round;  //points in current round
+   private int max;  //variable for max score in each round
    private static PairOfDice dice = new PairOfDice();
 
-   public Player (int roundMax){
-      total = 0;
-      round = 0;
-      max = roundMax;
+   public Player (int roundMax){ //constructor to define total, round, and max
+      total = 0; //sets total to 0 to start
+      round = 0; //sets round to 0 to start
+      max = roundMax; //sets max as the roundMax which is 20
    }
 
-   public boolean roll (PairOfDice dice, int goal){
-      boolean rollAgain = true;
-      Scanner scan = new Scanner (System.in);
-      dice.roll();
-      int die1 = dice.getDie1();
-      int die2 = dice.getDie2();
-      System.out.println ();
-      System.out.println ("Dice: " + die1 + " + " + die2 +
-                          " = " + (die1+die2));
-      if (die1 == 1 || die2 == 1){
-         System.out.println ("Busted!!!");
-         rollAgain = false;
-         round = 0;
-         if (die1 == 1 && die2 == 1) {
-            total = 0;
+   public boolean roll (PairOfDice dice, int goal){ //boolean roll method
+      boolean contRoll = true; //continue rolling boolean variable
+      Scanner scan = new Scanner(System.in); //Scanner for user input
+      dice.roll(); //run the roll method
+      int die1 = dice.getDie1(); //get die1 value
+      int die2 = dice.getDie2(); //get die2 value
+      System.out.println(); //space
+      System.out.println("Dice roll result: " + die1 + " + " + die2 + " = " + (die1+die2)); //prints out roll results
+      if (die1 == 1 || die2 == 1){ //if either die is 1
+         System.out.println("HA ROLLED ONE!!"); //message
+         contRoll = false; //roll stops and die goes to opponent
+         round = 0; //sets round to 0 because round is over
+         if (die1 == 1 && die2 == 1) { //if both die are 1
+            total = 0; //total score is 0
          }
-      }else{
-         round += die1 + die2;
-         System.out.println ("Current Round: " + round);
-         System.out.println ("Potential Total: " + (total+round));
-         if ((total+round) >= goal) {
-            rollAgain = false;
-         }else if (max == ASK){
-               System.out.print ("Take another turn (y/n)? ");
-               String again = scan.nextLine();
-               rollAgain = again.equalsIgnoreCase("y");
-            }else if (round >= max) {
-                  rollAgain = false;
+      }else{ //else
+         round += die1 + die2; //add die score to round score
+         System.out.println("Current Round: " + round); //print out round score
+         if ((total+round) >= goal) { //if total + round is greater than 100
+            contRoll = false; //continue roll is false
+         }else if (max == END){ //else if max round score is reached
+               System.out.print ("Roll again? (y for yes/n for no)? "); //asks user if they want to roll again
+               String sc = scan.nextLine(); //user input
+               contRoll = sc.equalsIgnoreCase("y"); //user inputs y for yes
+            }else if (round >= max) { //if the round score is greater than 20
+                  contRoll = false; //continue roll is false
             }
-         if (! rollAgain){
-            total += round;
-            round = 0;
+         if (contRoll == false){ //if continue roll is false
+            total += round; //add round to total
+            round = 0; //set round to 0
          }
       }
-      return rollAgain;
+      return contRoll; //return contRoll
    }
    
-   public int getPoints(){
-	   return total;
+   public int getPoints(){ //getPoints method
+	   return total; //returns total
    }
 }

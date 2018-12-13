67599de22bbc82c5f84bb11f8ab3ab6bf04d9c25
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2018-12-13 07:32:39 -0500

    Update PalindromeTester.java
    Latest Development

--- APCS-Chp8/src/chapter8/PalindromeTester.java
+++ APCS-Chp8/src/chapter8/PalindromeTester.java
@@ -1,7 +1,27 @@
 package chapter8;
 
 import java.util.Scanner;
-
 public class PalindromeTester { //PROJECT 8.1
 
+    public static boolean palindrome(String s) {
+        if(s.length() == 0 || s.length() == 1) {
+            return true;
+        }
+        
+        if(s.charAt(0) == s.charAt(s.length()-1)) {
+            return palindrome(s.substring(1, s.length()-1));
+        }
+        return false;
+    }
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Check if word is palindrome: ");
+        String pal = sc.nextLine();
+        if(palindrome(pal)) {
+            System.out.println(pal + " is a palindrome.");
+        }else {
+            System.out.println(pal + " is not a palindrome.");
+        }
+    }
 }
